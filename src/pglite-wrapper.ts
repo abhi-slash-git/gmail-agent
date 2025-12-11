516208d65d89a16fa7eeb726cc1882aa13c3260c
Abhi Patel <abhi@getlancey.com>
2025-12-10 20:28:38 -0500

    updating wrapper

--- src/pglite-wrapper.ts
+++ src/pglite-wrapper.ts
@@ -1,7 +1,7 @@
+import { readFile } from "node:fs/promises";
 import type { PGliteOptions } from "@electric-sql/pglite";
 import { PGlite } from "@electric-sql/pglite";
 import { live } from "@electric-sql/pglite/live";
-import { readFile } from "node:fs/promises";
 import dataPath from "./pglite-assets/pglite.data" with { type: "file" };
 import wasmPath from "./pglite-assets/pglite.wasm" with { type: "file" };
 
